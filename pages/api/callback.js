5d283ccfbf2fc45faf1bc7179e61a22bcfee25a1
Chirag Asarpota <hi@chiragasarpota.com>
2024-05-23 15:18:55 +0400

    bug: fixed callback handler issue with fetching body and incorrect success logic

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -5,51 +5,113 @@ import { sendBookingConfirmationEmail } from "../../utils/sendEmail";
 
 export default async function handler(req, res) {
   await dbConnect();
+  try {
+    console.log(`Callback Body: \n\n${req.body}`);
+    if (req.method === "POST") {
+      const {
+        id,
+        order_number,
+        order_amount,
+        order_currency,
+        order_description,
+        order_status,
+        type,
+        status,
+        payment_status,
+        reason,
+        payee_name,
+        payee_email,
+        payee_country,
+        payee_state,
+        payee_city,
+        payee_address,
+        rrn,
+        approval_code,
+        card,
+        card_expiration_date,
+        payee_card,
+        card_token,
+        customer_name,
+        customer_email,
+        customer_country,
+        customer_state,
+        customer_city,
+        customer_address,
+        customer_ip,
+        date,
+        recurring_init_trans_id,
+        recurring_token,
+        schedule_id,
+        exchange_rate,
+        exchange_rate_base,
+        exchange_currency,
+        exchange_amount,
+        vat_amount,
+        hash,
+      } = req.body;
 
-  if (req.method === "POST") {
-    const {
-      id,
-      order_number,
-      order_amount,
-      order_currency,
-      order_description,
-      order_status,
-      type,
-      status,
-      payment_status,
-      ...otherParams
-    } = req.body;
+      // Save the full payment response in the payments collection
+      const payment = new Payment({
+        id,
+        order_number,
+        order_amount,
+        order_currency,
+        order_description,
+        order_status,
+        type,
+        status,
+        payment_status,
+        reason,
+        payee_name,
+        payee_email,
+        payee_country,
+        payee_state,
+        payee_city,
+        payee_address,
+        rrn,
+        approval_code,
+        card,
+        card_expiration_date,
+        payee_card,
+        card_token,
+        customer_name,
+        customer_email,
+        customer_country,
+        customer_state,
+        customer_city,
+        customer_address,
+        customer_ip,
+        date,
+        recurring_init_trans_id,
+        recurring_token,
+        schedule_id,
+        exchange_rate,
+        exchange_rate_base,
+        exchange_currency,
+        exchange_amount,
+        vat_amount,
+        hash,
+      });
 
-    // Save the full payment response in the payments collection
-    const payment = new Payment({
-      id,
-      order_number,
-      order_amount,
-      order_currency,
-      order_description,
-      order_status,
-      type,
-      status,
-      payment_status,
-      ...otherParams,
-    });
+      await payment.save();
 
-    await payment.save();
+      // Check payment status and update order if settled
+      if (status === "success" && type === "sale") {
+        await Order.updateOne(
+          { _id: order_number },
+          { $set: { status: "PAID" } }
+        );
+        await sendBookingConfirmationEmail(order_number);
+      }
 
-    // Check payment status and update order if settled
-    if (payment_status === "settled") {
-      await Order.updateOne(
-        { _id: order_number },
-        { $set: { status: "PAID" } }
-      );
-      await sendBookingConfirmationEmail(order_number);
+      res.status(200).json({
+        message: "Callback received successfully",
+        paymentId: payment.id,
+      });
+    } else {
+      res.status(405).json({ error: "Method Not Allowed" });
     }
-
-    res.status(200).json({
-      message: "Callback received successfully",
-      paymentId: payment.id,
-    });
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+  } catch (error) {
+    console.log(`Error in callback - ${error}`);
   }
 }
